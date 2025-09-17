96e0e20eb23057a776f2a062812a8bb326c45fa1
Sara1garcia <sv_garciag@javeriana.edu.co>
2025-09-17 09:54:16 -0500

    archivos llenos

--- frontend/frontend/src/app/mascotas/mascota-formulario/mascota-formulario.component.ts
+++ frontend/frontend/src/app/mascotas/mascota-formulario/mascota-formulario.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MascotasService } from '../mascotas.service';
+import { Mascota } from '../mascota';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-mascota-formulario',
   standalone: false,
-  templateUrl: './mascota-formulario.html',
-  styleUrl: './mascota-formulario.css'
+  templateUrl: './mascota-formulario.component.html',
+  styleUrl: './mascota-formulario.component.css'
 })
-export class MascotaFormulario {
 
+export class MascotaFormularioComponent implements OnInit { 
+  mascota: Mascota = {};
+
+  constructor (private mascotasService: MascotasService,
+               private route: ActivatedRoute,
+               private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.mascotasService.getMascotaById(+id).subscribe(mascota => {
+        if (mascota) {
+          this.mascota = mascota;
+        } else {
+          this.router.navigate(['/mascotas']);
+        }
+      });
+    }
+  }
+
+  guardarMascota(): void {
+    if (this.mascota.id) {
+      this.mascotasService.updateMascota(this.mascota).subscribe(() => {
+        this.mascotasService.notificarMascotaAgregada();
+        this.router.navigate(['/mascotas']);
+      });
+    } else {
+      this.mascotasService.addMascota(this.mascota).subscribe(() => {
+      this.mascotasService.notificarMascotaAgregada();
+      this.router.navigate(['/mascotas']);
+      });
+    }
+  }
 }
--- frontend/frontend/src/app/mascotas/mascota-tabla/mascota-tabla.component.ts
+++ frontend/frontend/src/app/mascotas/mascota-tabla/mascota-tabla.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Mascota } from '../mascota';
+import { MascotasService } from '../mascotas.service';
 
 @Component({
   selector: 'app-mascota-tabla',
   standalone: false,
-  templateUrl: './mascota-tabla.html',
-  styleUrl: './mascota-tabla.css'
+  templateUrl: './mascota-tabla.component.html',
+  styleUrl: './mascota-tabla.component.css'
 })
-export class MascotaTabla {
+export class MascotaTablaComponent implements OnInit {
+  mascotas: Mascota[] = [];
 
+  constructor(private mascotasService: MascotasService) { }
+
+  ngOnInit(): void {
+    this.getAllMascotas();
+    this.mascotasService.mascotaAgregada$.subscribe(() => {
+      this.getAllMascotas();
+    });
+  }
+
+  getAllMascotas(): void {
+    this.mascotasService.getAllMascotas()
+      .subscribe(
+        (data: Mascota[]) => {
+          this.mascotas = data;
+        },
+        (error) => {
+          console.error('Error getting mascotas', error);
+        }
+      );
+  }
+
+  onDelete(id: number | undefined): void {
+    if (id !== undefined) {
+      this.mascotasService.deleteMascota(id).subscribe(() => {
+        this.getAllMascotas();
+      });
+    }
+  }
 }
